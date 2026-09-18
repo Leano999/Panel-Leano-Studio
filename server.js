5760be777c176dbf95e283ddc85500b34c1ac065
Leano999 <alpinleon1@gmail.com>
2026-09-18 02:16:51 -0700

    Panel-Leano V1.66

--- server.js
+++ server.js
@@ -4,33 +4,27 @@ const archiver = require("archiver");
 
 const app = express();
 app.use(express.json());
-
-app.post("/download", async (req, res) => {
-  try {
-    let ids = (req.body.ids || []).slice(0, 10);
-
-    res.setHeader("Content-Type", "application/zip");
-    res.setHeader("Content-Disposition", "attachment; filename=assets.zip");
-
-    const archive = archiver("zip");
-    archive.pipe(res);
-
-    for (let id of ids) {
-      try {
-        const url = `https://assetdelivery.roblox.com/v1/asset/?id=${id}`;
-        const file = await axios.get(url, { responseType: "arraybuffer", maxRedirects: 5 });
-        archive.append(file.data, { name: `${id}.mp3` });
-      } catch (e) {
-        console.log("Gagal:", id);
-      }
-    }
-
-    await archive.finalize();
-  } catch (err) {
-    res.status(500).send("Error");
-  }
-});
-
 app.use(express.static("public"));
 
-app.listen(3000, () => console.log("Server jalan di 3000"));
+app.post("/download", async (req, res) => {
+  let ids = (req.body.ids || []).slice(0, 10);
+
+  res.setHeader("Content-Type", "application/zip");
+  res.setHeader("Content-Disposition", "attachment; filename=assets.zip");
+
+  const archive = archiver("zip");
+  archive.pipe(res);
+
+  for (let i = 0; i < ids.length; i++) {
+    let id = ids[i];
+    try {
+      const url = `https://assetdelivery.roblox.com/v1/asset/?id=${id}`;
+      const file = await axios.get(url, { responseType: "arraybuffer" });
+      archive.append(file.data, { name: `${id}.mp3` });
+    } catch {}
+  }
+
+  archive.finalize();
+});
+
+app.listen(3000, () => console.log("Running on 3000"));
